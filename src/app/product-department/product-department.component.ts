7d35fa0a360740c3d9dc87cf3be37c9334d86510
Jonathan Wilkison <jwilkis2@gmail.com>
2017-09-30 23:31:55 -0500

    Completed Product Department Component

--- src/app/product-department/product-department.component.ts
+++ src/app/product-department/product-department.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductDepartment: A component to show the breadcrumbs to a
+ * Product's department
+ */
 @Component({
-  selector: 'app-product-department',
-  templateUrl: './product-department.component.html',
-  styleUrls: ['./product-department.component.css']
+  selector: 'product-department',
+  templateUrl: './product-department.component.html'
 })
-export class ProductDepartmentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductDepartmentComponent {
+  @Input() product: Product;
 }
