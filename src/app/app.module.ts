8310c1bc413cc249284810dff7499ea001cfbb21
Jonathan Wilkison <jwilkis2@gmail.com>
2017-09-30 23:26:02 -0500

    Finished Price Display Component and created Product Department Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductRowComponent,
-    ProductImageComponent
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
     BrowserModule,
