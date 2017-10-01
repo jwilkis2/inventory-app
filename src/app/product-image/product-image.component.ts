4b9b4acf68cb8edaf0f5f6430bb4083ba1dde4f8
Jonathan Wilkison <jwilkis2@gmail.com>
2017-09-30 23:22:02 -0500

    Created Product Image Component

--- src/app/product-image/product-image.component.ts
+++ src/app/product-image/product-image.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+  Input,
+  HostBinding
+} from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
-  selector: 'app-product-image',
-  templateUrl: './product-image.component.html',
-  styleUrls: ['./product-image.component.css']
+  selector: 'product-image',
+  template: `
+  <img class="product-image" [src]="product.imageUrl">
+  `
 })
-export class ProductImageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductImageComponent {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
 }
